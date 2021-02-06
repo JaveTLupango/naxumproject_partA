c349b591984b7279a0af10ba766a747b356541db
Jave T Lupango <jtlupango@gmail.com>
2021-02-06 16:23:00 +0800

    first commit

---
+++ app/Http/Controllers/CommissionController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Commission;
+use Illuminate\Http\Request;
+
+class CommissionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $comm = Commission::latest()->pagination(5);
+        return view('commission.index', compact('comm'))->with('i', (request()->input('page', 1) - 1) * 5);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return view('Commission.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Commission  $commission
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Commission $commission)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Commission  $commission
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Commission $commission)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Commission  $commission
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Commission $commission)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Commission  $commission
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Commission $commission)
+    {
+        //
+    }
+}
---
+++ resources/views/commission/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+@section(content);
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="full-right">
+                <h1> Commission Report </h1>
+            </div>
+        </div>
+    </div>
+@endsection
